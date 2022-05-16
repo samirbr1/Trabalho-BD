46136a414bba038ba76602f9ed8fa23246cd9f37
Samir Murad <samirbmurad@gmail.com>
2022-05-16 09:04:37 -0400

    Update Trabalho_BD.sql

--- Trabalho_BD.sql
+++ Trabalho_BD.sql
@@ -11,20 +11,28 @@ Alunos: Fabricio Fogaça da Silva Lemos, Felipe Emanuel Ferreira, Samir Barcelos
 CREATE TABLE Turma
 (
     idTurma INT,
+    idCurso INT,
+    idProfessor INT,	
     Descricao VARCHAR(100),
     data_inicio DATE, 
     data_termino DATE,
-    valor DECIMAL(10, 2),
-    idProfessor INT REFERENCES Professor(idProfessor),
-    idCurso INT REFERENCES Curso(idProfessor),
+    idProfessor INT,
+    FOREIGN KEY (idProfessor) REFERENCES Professor(idProfessor)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idCurso) REFERENCES Curso(idCurso)
+		ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY(idTurma)
 );
 
 CREATE TABLE diasAulaTurma
 (
-    idTurma INT REFERENCES Turma(idTurma),
-    idDiaAula INT REFERENCES DiadeAula(idDia),
+    idTurma INT,
+    idDiaAula INT,
     Horario VARCHAR(50),    /*ex: 9:15 - 10:50*/
+    FOREIGN KEY (idTurma) REFERENCES Turma(idTurma)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idDiaAula) REFERENCES DiadeAula(idDia)
+		ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY (idTurma, idDiaAula)
 );
 
@@ -38,34 +46,27 @@ CREATE TABLE DiadeAula
 CREATE TABLE turmaEstudante
 (
     idTurmaestudante INT,
-    idTurma INT REFERENCES Turma(idTurma),
-    idEstudante INT REFERENCES Estudante(idEstudante),
+    idTurma INT,
+    idEstudante INT REFERENCES,
+    FOREIGN KEY (idTurma) REFERENCES Turma(idTurma)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY	 (idEstudante) REFERENCES Estudante(idEstudante)
+		ON DELETE CASCADE ON UPDATE CASCADE,
     PRIMARY KEY(idTurmaestudante)
 );
 
 CREATE TABLE Professor
 (
     idProfessor INT,
-    Descricao VARCHAR(300),
-    Pnome VARCHAR(20),
-    Minicial CHAR,
-    Unome VARCHAR(20),
-    Sexo CHAR,
-    Salario DECIMAL(10,2),
-    Cpf CHAR(11) NOT NULL,
-    Datanasc DATE,
-    Email VARCHAR(50),
-    celular VARCHAR(15),
-    endereco VARCHAR(100) NOT NULL,
+    Curriculo VARCHAR(300),
     PRIMARY KEY(idProfessor)
 );
 
 CREATE TABLE Estudante
 (
-    idEstudante INT PRIMARY KEY,
+    idEstudante INT,
     Cpf CHAR(11),
     datanasc DATE,
-    CEP char(9),
     Endereco VARCHAR(100),
     Pnome VARCHAR(20),
     Minicial CHAR,
@@ -81,19 +82,27 @@ CREATE TABLE Curso
     idCurso INT,
     Aulas INT,
     Descricao VARCHAR(300),
-    term VARCHAR(100),  /*não sei*/
+    periodoEletivo VARCHAR(100),
     PRIMARY KEY(idCurso)
 );
 
 CREATE TABLE Pagamento
 (
-    idPagamento  INT PRIMARY KEY,
+    idPagamento  INT,
+    idEstudante INT,
+    idTurma INT, 
+    idMetodopagamento INT,
     dataPagamento DATETIME,
     Quantia  DECIMAL(10, 2),
-    idMetodopagamento INT REFERENCES MetodoPagamento(idMetodopagamento),
     statusPagamento VARCHAR(20),
-    idEstudante INT REFERENCES Estudante(idEstudante),
-    idTurma INT REFERENCES Turma(idEstudante)
+    
+    FOREIGN KEY (idMetodopagamento) REFERENCES MetodoPagamento(idMetodopagamento)	
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idEstudante) REFERENCES Estudante(idEstudante)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (idTurma) REFERENCES Turma(idEstudante)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY(idPagamento)	
 );
 
 CREATE TABLE MetodoPagamento
@@ -116,7 +125,8 @@ CREATE TABLE Funcionario
     Salario DECIMAL(10,2),
     email VARCHAR(50),
     celular VARCHAR(15),
-	PRIMARY KEY(idFuncionario)
+    endereco VARCHAR(50),
+    PRIMARY KEY(idFuncionario)
 );
 
 
